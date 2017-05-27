201fcb52efb97a27cc24d3ea8a0212d29866ab58
Gabriel Lima <gabrielcastroslima@gmail.com>
2017-05-27 18:58:08 -0300

    Cria crawler do Superprix

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import json
+import codecs
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+# class TutorialPipeline(object):
+    # def process_item(self, item, spider):
+    #     line = json.dumps(dict(item)) + "\n"
+    #     return item
+
 class TutorialPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('items.jl', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
