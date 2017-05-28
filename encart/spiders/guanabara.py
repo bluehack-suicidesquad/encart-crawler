45c0d8d54b805ce6aa0ef3df8d202a800e33cf50
Lucas Trindade <lucasktrindade@gmail.com>
2017-05-28 11:21:58 -0300

    Adicionando guanabara category e market

--- encart/spiders/guanabara.py
+++ encart/spiders/guanabara.py
@@ -9,11 +9,32 @@ class GuanabaraSpider(scrapy.Spider):
         'http://www.supermercadosguanabara.com.br/produtos/4',
         'http://www.supermercadosguanabara.com.br/produtos/5',
         'http://www.supermercadosguanabara.com.br/produtos/6',
-        'http://www.supermercadosguanabara.com.br/produtos/7'
-
+        'http://www.supermercadosguanabara.com.br/produtos/7',
+        'http://www.supermercadosguanabara.com.br/produtos/8',
+        'http://www.supermercadosguanabara.com.br/produtos/9',
+        'http://www.supermercadosguanabara.com.br/produtos/10',
+        'http://www.supermercadosguanabara.com.br/produtos/11',
+        'http://www.supermercadosguanabara.com.br/produtos/12',
+        'http://www.supermercadosguanabara.com.br/produtos/13',
+        'http://www.supermercadosguanabara.com.br/produtos/14',
+        'http://www.supermercadosguanabara.com.br/produtos/15',
+        'http://www.supermercadosguanabara.com.br/produtos/16',
+        'http://www.supermercadosguanabara.com.br/produtos/17',
+        'http://www.supermercadosguanabara.com.br/produtos/18',
+        'http://www.supermercadosguanabara.com.br/produtos/19',
+        'http://www.supermercadosguanabara.com.br/produtos/20',
+        'http://www.supermercadosguanabara.com.br/produtos/21',
+        'http://www.supermercadosguanabara.com.br/produtos/22',
+        'http://www.supermercadosguanabara.com.br/produtos/23',
+        'http://www.supermercadosguanabara.com.br/produtos/24',
+        'http://www.supermercadosguanabara.com.br/produtos/25',
+        'http://www.supermercadosguanabara.com.br/produtos/26'
     ]
 
     def parse(self, response):
+        self.category = ''
+
+        self.category = response.css('div.products-bar div.title h3::text').extract_first()
         for item in response.css('div.products-list div.item'):
             yield {
                 'name': item.css('div.name::text')
@@ -24,4 +45,6 @@ class GuanabaraSpider(scrapy.Spider):
                     .replace('\');',''),
                 'price': item.css('span.number::text')
                     .extract_first(),
+                'category': self.category,
+                'market': 'Guanabara'
             }
