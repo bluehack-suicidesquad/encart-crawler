dea256228ba4cd618e34829b5e659eda63e5c319
Gabriel Lima <gabrielcastroslima@gmail.com>
2017-05-27 22:06:42 -0300

    Faz com que venha todos os items do crawler da Superprix

--- tutorial/spiders/superprix.py
+++ tutorial/spiders/superprix.py
@@ -6,7 +6,7 @@ class SuperprixSpider(scrapy.Spider):
     name = "superprix"
     start_urls = [
         'http://www.superprix.com.br/carnes-e-pescados/',
-        'http://www.superprix.com.br/bebidas'
+        'http://www.superprix.com.br/bebidas/'
     ]
 
     def parse(self, response):
@@ -15,16 +15,16 @@ class SuperprixSpider(scrapy.Spider):
 
         self.category = response.css('.bread-crumb li.last a::attr(title)').extract_first()
 
-        for item in response.css('div.prateleira ul li'):
+        for item in response.css('div.prateleira > ul > li:not(.helperComplement)'):
             yield {
                 'name': item.css('a.productImage::attr(title)')
                     .extract_first(),
                 'image': item.css('a.productImage img::attr(src)')
                     .extract_first(),
-                'price': item.css('span.newPrice em')
-                    .extract_first()
-                    .replace('<em>R$ ','')
-                    .replace('</em>',''),
+                'price': item.css('div.data .newPrice em').extract_first(),
+                    # item.css('span.centavos').extract_first(),
+                    # .replace('<em>R$ ','')
+                    # .replace('</em>',''),
                 'market': 'superprix',
                 'category': self.category
             }
