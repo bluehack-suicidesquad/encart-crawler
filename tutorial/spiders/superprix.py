4b52e608e2f90d2a2b073f20e0a47e460bf8f403
Gabriel Lima <gabrielcastroslima@gmail.com>
2017-05-27 21:05:34 -0300

    Categoriza os items

--- tutorial/spiders/superprix.py
+++ tutorial/spiders/superprix.py
@@ -5,13 +5,24 @@ import scrapy
 class SuperprixSpider(scrapy.Spider):
     name = "superprix"
     start_urls = [
-        'http://www.superprix.com.br/carnes-e-pescados'
+        'http://www.superprix.com.br/carnes-e-pescados/',
+        'http://www.superprix.com.br/bebidas'
     ]
 
     def parse(self, response):
-        for item in response.css('div.prateleira'):
+        yield {
+            'category': response.css('.bread-crumb li.last a::attr(title)')
+                .extract_first()
+        }
+        for item in response.css('div.prateleira ul'):
             yield {
-                'name': item.css('a.productImage::attr(title)').extract_first(),
-                'image': item.css('a.productImage img::attr(src)').extract_first(),
-                'price': item.css('span.newPrice em').extract_first().replace('<em>R$ ','').replace('</em>','')
+                'name': item.css('a.productImage::attr(title)')
+                    .extract_first(),
+                'image': item.css('a.productImage img::attr(src)')
+                    .extract_first(),
+                'price': item.css('span.newPrice em')
+                    .extract_first()
+                    .replace('<em>R$ ','')
+                    .replace('</em>',''),
+                'market': 'superprix'
             }
